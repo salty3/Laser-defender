ba8aa47a5d955b774225581d879347ddc2be578e
salty3 <mistick115@gmail.com>
2020-07-25 16:14:23 +0300

    Enemy pathing implement

--- Laser Defender/Assets/EnemyPathing.cs
+++ Laser Defender/Assets/EnemyPathing.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
 
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] private List<Transform> waypoints;
+    [SerializeField] private float moveSpeed = 2f;
+
+    private int waypointIndex = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPos = waypoints[waypointIndex].position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, movementThisFrame);
+
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
