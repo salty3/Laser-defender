9d113141a7b75daa4f81542abb291223af6b262b
salty3 <mistick115@gmail.com>
2020-07-25 17:32:39 +0300

    Enemy spawn implemented

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] private List<WaveConfig> waveConfigs;
+    private int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnWaveEnemies(currentWave));
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnWaveEnemies(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            Instantiate(
+                waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[enemyCount].position,
+                Quaternion.identity);
+            yield return new WaitForSeconds(
+                waveConfig.GetTimeBetweenSpawns() + 
+                UnityEngine.Random.Range(
+                    -waveConfig.GetSpawnRandomFactor(), 
+                    waveConfig.GetSpawnRandomFactor()
+                    )
+                ); 
+        }
+    }
+}
